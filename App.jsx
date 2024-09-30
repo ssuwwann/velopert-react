6e8873867753455f569fe6d40dfb515ca65ad585
dduwan <leesuwan89@gmail.com>
2024-09-30 20:33:38 +0900

    scss 맛보기

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import SassComponent from './components/SassComponent';
+
+function App() {
+  return (
+    <>
+      <SassComponent />
+    </>
+  );
+}
+
+export default App;
